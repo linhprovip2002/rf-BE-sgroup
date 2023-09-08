d0c1e684643c649401809f6d0c49da0004a22fea
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2023-09-08 20:57:42 +0700

    nginx

--- src/config/knex.ts
+++ src/config/knex.ts
@@ -6,7 +6,7 @@ const config = {
   client: 'mysql',
   connection: {
     host: process.env.DATABASE_HOST,
-    port: 3306,
+    port: 3318,
     user: 'root',
     password: process.env.PASSWORD,
     database: process.env.DATABASE
